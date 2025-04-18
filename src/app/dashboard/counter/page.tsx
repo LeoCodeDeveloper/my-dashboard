572772fba39eeb3c1a09ea164498697e5bcf1223
leoTechnologyDeveloper <www.leotechnology@gmail.com>
2025-04-18 09:25:35 -0500

    added description metadadata

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -5,7 +5,7 @@ import CartCounter from "@/app/shopping-cart/components/CartCounter";
 
 export const metadata = {
   title: "Shopping cart",
-  description: "yeaaaa el cart"
+  description: "El Shopping cart de la Dashboard 1"
 }
 
 
@@ -19,7 +19,7 @@ export default function CounterPage() {
       <main className="flex flex-col items-center justify-center   w-full h-full ">
         <span>Productos en el carrito</span>
       {/* <CartCounter /> */}
-      <CartCounter value={20}/>
+      <CartCounter value={200}/>
       </main>
  
     
